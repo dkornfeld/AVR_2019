32228ffb5e2564c4484725e3de02c1d79878bb2c
FP <fp@fp.fp>
2019-02-09 17:48:32 -0800

    Remove 16 bits of unnecessary signals

--- HW4/Registers.vhd
+++ HW4/Registers.vhd
@@ -36,6 +36,7 @@
 --        02/02/19    Bobby Abrahamson        Updated to output SREG for HW3
 --        02/05/19    David Kornfeld          Fixed Interrupt flag sensitivity
 --        02/07/19    David Kornfeld          Added 16-bit register bus and reset on SP
+--        02/08/19    Bobby Abrahamson        Increased select width, support for IO r/w
 -----------------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -85,19 +86,13 @@ architecture data_flow of Registers is
     
     --Define registers, signals.
     type REG_ARRAY is array (0 to NUM_REGS-1) of std_logic_vector(NUM_BITS-1 downto 0);
-    signal RegData  :    REG_ARRAY := (others => (others => '0')); -- For simulation only. Init to 0
-    signal RegA     :    std_logic_vector(NUM_BITS-1 downto 0); -- used for intermediate computations, 
-                                                                -- will be useful for future assignments
-    signal RegB     :    std_logic_vector(NUM_BITS-1 downto 0); -- used for intermediate computations, 
-                                                                -- will be useful for future assignments
+    signal RegData  :    REG_ARRAY := (others => (others => '0')); -- For simulation only, init to 0
 begin
     -- Handle RegA
-    RegA <= RegData(conv_integer(RegASel));
-    RegAOutput <= RegA when SFlag = '0' else RegData(SREG_IDX);
+    RegAOutput <= RegData(conv_integer(RegASel));
     
     -- Handle RegB
-    RegB <= RegData(conv_integer(RegBSel));
-    RegBOutput <= RegB;
+    RegBOutput <= RegData(conv_integer(RegBSel));
     
     -- Output SREG
     SREG <= RegData(SREG_IDX);
@@ -132,19 +127,23 @@ begin
             end if;
             -- Update Address registers as requested
             if (AddrRegWr = '1') then
-                if (AddrRegSel = ADDR_REG_SEL_X) then     -- Write to X
+                if (AddrRegSel = ADDR_REG_SEL_X) then
+                    -- Write to X
                     RegData(X_LOW) <= AddrRegIn(NUM_BITS-1 downto 0);
                     RegData(X_HIGH) <= AddrRegIn(DATA_AB_SIZE-1 downto NUM_BITS);
                 end if;
-                if (AddrRegSel = ADDR_REG_SEL_Y) then  -- Write to Y
+                if (AddrRegSel = ADDR_REG_SEL_Y) then
+                    -- Write to Y
                     RegData(Y_LOW) <= AddrRegIn(NUM_BITS-1 downto 0);
                     RegData(Y_HIGH) <= AddrRegIn(DATA_AB_SIZE-1 downto NUM_BITS);
                 end if;
-                if (AddrRegSel = ADDR_REG_SEL_Z) then  -- Write to Z
+                if (AddrRegSel = ADDR_REG_SEL_Z) then
+                    -- Write to Z
                     RegData(Z_LOW) <= AddrRegIn(NUM_BITS-1 downto 0);
                     RegData(Z_HIGH) <= AddrRegIn(DATA_AB_SIZE-1 downto NUM_BITS);
                 end if;
-                if (AddrRegSel = ADDR_REG_SEL_SP) then  -- Write to SP
+                if (AddrRegSel = ADDR_REG_SEL_SP) then
+                    -- Write to SP
                     RegData(SP_LOW) <= AddrRegIn(NUM_BITS-1 downto 0);
                     RegData(SP_HIGH) <= AddrRegIn(DATA_AB_SIZE-1 downto NUM_BITS);
                 end if;
